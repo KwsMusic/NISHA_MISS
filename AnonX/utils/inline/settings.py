63738884f556dddb1f19e77f17fff06a11f94f7a
UNKNOWN-MUSIC <130531308+KwsMusic@users.noreply.github.com>
2023-10-23 03:52:48 +0530

    Update settings.py
    Signed-off-by: UNKNOWN-MUSIC <130531308+KwsMusic@users.noreply.github.com>

--- AnonX/utils/inline/settings.py
+++ AnonX/utils/inline/settings.py
@@ -24,7 +24,7 @@ def setting_markup(_):
         ],
         [
             InlineKeyboardButton(
-                text="• sᴜᴩᴩᴏʀᴛ •", url=f"{SUPPORT_GROUP}",
+                text="• 🍒̵̶⃰͟˶֟፝͟͝sᴜᴩᴩᴏʀᴛ⏎͟🝛꯭ •", url=f"{SUPPORT_GROUP}",
             ),
             InlineKeyboardButton(
                 text=_["CLOSE_BUTTON"], callback_data="close"
